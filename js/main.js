872ce7659feab76b030683cdc25a294f9634e957
Александр <mas2005@inbox.ru>
2021-05-29 16:02:34 +0300

    Выполнил задание 2 - Нужно больше функций - дополнения

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function getRandomIntInclusivePositive(min, max) {
   if (min > max) {
-    console.log('Введите правильный диапазон');
+    return min, max;
   } else if (min === max) {
-    console.log('Числа в диапазоне должны быть разными');
+    return min, max;
   } else if (min < max) {
     min = Math.abs(Math.ceil(min));
     max = Math.abs(Math.floor(max));
@@ -15,15 +15,15 @@ getRandomIntInclusivePositive(5,10);
 
 function getRandomInclusiveAfterDecimal (min, max, numbersAfterDecimal) {
   if (min > max) {
-    console.log('Введите правильный диапазон');
+    return min, max;
   } else if (min === max) {
-    console.log('Числа в диапазоне должны быть разными');
+    return min, max;
   } else if (min < max) {
     min = Math.abs(Math.ceil(min));
     max = Math.abs(Math.floor(max));
-    let randomNumber = Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
+    const randomNumber = Math.random() * (max - min + 1) + min; //Максимум и минимум включаются
     return parseFloat(randomNumber.toFixed(numbersAfterDecimal));
-  };
+  }
 }
 
 getRandomInclusiveAfterDecimal(1, 20, 5);
